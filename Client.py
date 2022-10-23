9591c4056ff2c0139a238e64220797964d721bde
Flife11 <73180106+Flife11@users.noreply.github.com>
2022-10-23 07:46:13 +0700

    Fail on 2 html test case

--- Client.py
+++ Client.py
@@ -1,31 +1,44 @@
 import socket
 
 CLIENT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-URL = "web.stanford.edu"
-fileName = "01-intro.pdf"
-# URL = "www.example.com"
-# fileName = "index.html"
+CLIENT.setsockopt( socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+CLIENT.setsockopt(socket.SOL_TCP, socket.TCP_KEEPIDLE, 60)
+        # overrides value shown by sysctl net.ipv4.tcp_keepalive_probes
+CLIENT.setsockopt(socket.SOL_TCP, socket.TCP_KEEPCNT, 4)
+        # overrides value shown by sysctl net.ipv4.tcp_keepalive_intvl
+CLIENT.setsockopt(socket.SOL_TCP, socket.TCP_KEEPINTVL, 15)
+url = "http://web.stanford.edu/class/cs231a/project.html"
+class constant:
+    def __init__(self, url):
+        index = 0
+        if (url.find("http://")>=0) :
+            index = 1 
+        if (url[len(url)-1]!='/' and len(url.split("http://"))==1): url = url + '/'
+        path = url.split("http://")[index].split("/")
+        self.link = path[0]
+        self.fileName = path[len(path)-1]
+        if (self.fileName=="") : 
+            self.fileName = "index.html"
+        path.pop(0)
+        self.tag = '/'.join(path)
 
-def _getIP(LINK):
-    return socket.gethostbyname(LINK)
 
-def _const(LINK):
-    host = "Host: {}\r\n".format(LINK)
+def _getIP(link):
+    return socket.gethostbyname(link)
+
+def _constHeader(link):
+    host = "Host: {}\r\n".format(link)
     connection = "Connection: Keep-Alive\r\n"
     return [host, connection]
 
 
-def _message():
-    LINK = URL
-    TAG = "class/cs224w/slides/01-intro.pdf"
-    # TAG = ""
-    const = _const(LINK)
-    
-    MESSAGE = "GET /{} HTTP/1.1\r\n{}\r\n".format(TAG, "".join(const))
+def _message(const):
+    constHeader = _constHeader(const.link)
+    MESSAGE = "GET /{} HTTP/1.1\r\n{}\r\n".format(const.tag, "".join(constHeader))
     return MESSAGE
 
-def _connect():
-    IP = _getIP(URL)
+def _connect(const):
+    IP = _getIP(const.link)
     PORT = 80
     try:
         print(f"Connecting to {IP}:{PORT}")
@@ -38,45 +51,60 @@ def _connect():
 
 
 def _getContentLength(responde):
+    if (responde.find("Content-Length: ")==-1) : return -1
     tmp = responde.split("Content-Length: ")[1].split('\r\n')
     return int(tmp[0])
 
-def _sendRequest():
-    f = open(fileName, "w", encoding="latin-1")
-    dataLen = 1024*1024
-    print(dataLen)
-    MESSAGE = _message()
-    print(MESSAGE)
-    CLIENT.send(MESSAGE.encode())
-    data = CLIENT.recv(dataLen)
 
-    responde = data.decode("latin-1").split("\r\n\r\n")[0]
-    ContentLength = _getContentLength(responde)
-    DATA = data.decode("latin-1").split("\r\n\r\n")[1]
-    
+def _sendRequestWithChunked():
+    pass
+
+def _sendRequestWithContentLength(fileName, ContentLength, data):
+    f = open(fileName, "wb")
+    dataLen = 1024*1024
+
     try:
         while (True):        
-            d = '\n'.join(DATA.split("\r\n"))
-            f.write(d)
-            ContentLength -= len(DATA)
-            print(len(data), len(DATA), len(d), ContentLength)
+            print(ContentLength, len(data))
+            f.write(data)
+            ContentLength -= len(data)
             if ContentLength==0:
                 break
+            print(ContentLength, len(data))
             data = CLIENT.recv(dataLen)
-            DATA = data.decode("latin-1")
-    except:
-        print("ERROR")
-    
+    except socket.error as e:
+        print(f"Socket error: {e}")
     f.close()
     CLIENT.close()
 
 
+def _sendRequest(const):
+    dataLen = 1024*1024
+    Message = _message(const)
+    CLIENT.send(Message.encode())
+    data = CLIENT.recv(dataLen)
+    
+    responde = data.decode("latin-1").split("\r\n\r\n")[0]
+    ContentLength = _getContentLength(responde)
+    D = data.decode("latin-1").split("\r\n\r\n")[1]
+
+    print(responde)
+
+    if (ContentLength==-1) :
+        _sendRequestWithChunked()
+    else : _sendRequestWithContentLength(const.fileName, ContentLength, D.encode("latin-1"))
+
+    
+
 def _main():
-    _connect()
-    _sendRequest()
+    const = constant(url)
+    # print(const.fileName)
+    # print(const.link)
+    # print(const.tag)
+    _connect(const)
+    _sendRequest(const)
 
 _main()
-
 
 
 
